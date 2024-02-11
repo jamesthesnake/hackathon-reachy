fa10fd474322bdd29ca943899acd6d9555947043
James Hennessy <james.ryan.hennessy@gmail.com>
2024-02-10 18:04:14 -0800

    bump

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
+from octo.model.octo_model import OctoModel
+from PIL import Image
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
 
+model = OctoModel.load_pretrained("hf://rail-berkeley/octo-small")
 print("Start recording audio")
 sample_name = "aaaa.wav"
 cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
@@ -13,4 +19,11 @@ import cv2
 camera_capture = cv2.VideoCapture(0)
 rv, image = camera_capture.read()
 print(f"Image Dimensions: {image.shape}")
+img = np.array(Image.open(requests.get(image, stream=True).raw).resize((256, 256)))
+plt.imshow(img)
+img = img[np.newaxis,np.newaxis,...]
+observation = {"image_primary": img, "pad_mask": np.array([[True]])}
+task = model.create_tasks(texts=["pick up the ball"])
+action = model.sample_actions(observation, task, rng=jax.random.PRNGKey(0))
+print(action)   # [batch, action_chunk, action_dim]
 camera_capture.release()
